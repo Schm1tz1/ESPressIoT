ad87897e2a2e294889e9a54bca391e8de371e977
Billy <34207387+PouryaParsa@users.noreply.github.com>
2024-01-15 14:58:51 +0330

    Update mqtt.ino

--- espressiot/mqtt.ino
+++ espressiot/mqtt.ino
@@ -1,36 +1,27 @@
-//
 // ESPressIoT Controller for Espresso Machines
 // 2016-2021 by Roman Schmitz
 //
 // MQTT integration
-//
-
-#define MQTT_DEBUG
 
 #ifdef ENABLE_MQTT
 
-char buf_msg[50];
-
-#include <SPI.h>
-#include <Ethernet.h>
 #include <WiFiClient.h>
-
 #include <PubSubClient.h>
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-#define concat(first, second) first second
-const char *mqttConfigTopic = concat(MQTT_TOPIC, "/config/#");
-const char *mqttStatusTopic = concat(MQTT_TOPIC, "/status");
-
+const char* mqttConfigTopic = MQTT_TOPIC "/config/#";
+const char* mqttStatusTopic = MQTT_TOPIC "/status";
 
 void MQTT_reconnect() {
-  if (!client.connected()) {
+  // Retry connection to MQTT broker
+  for (int i = 0; i < MAX_CONNECTION_RETRIES && !client.connected(); i++) {
     Serial.print("Attempting MQTT connection...");
-
+    
     String clientId = "ESP8266Client";
     clientId += String(random(0xffff), HEX);
+    
     if (client.connect(clientId.c_str(), MQTT_USER, MQTT_PASS)) {
       Serial.println("connected");
       client.subscribe(mqttConfigTopic, 1);
@@ -42,49 +33,46 @@ void MQTT_reconnect() {
 }
 
 void MQTT_callback(char* topic, byte* payload, unsigned int length) {
-#ifdef MQTT_DEBUG
+  // Process incoming MQTT messages
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] '");
-#endif MQTT_DEBUG
+
   String msg = "";
   for (int i = 0; i < length; i++) {
-#ifdef MQTT_DEBUG
     Serial.print((char)payload[i]);
-#endif MQTT_DEBUG
-    msg += (char) payload[i];
+    msg += (char)payload[i];
   }
-#ifdef MQTT_DEBUG
   Serial.println("'");
-#endif MQTT_DEBUG
 
   double val = msg.toFloat();
   Serial.println(val);
 
+  // Handle specific topics
   if (strstr(topic, "/config/tset")) {
-    if (val > 1e-3) gTargetTemp = val;
+    if (val > 1e-3) {
+      gTargetTemp = val;
+    }
+  } else if (strstr(topic, "/config/toggle")) {
+    poweroffMode = !poweroffMode;
   }
-  else if (strstr(topic, "/config/toggle")) {
-    poweroffMode = (!poweroffMode);
-  }
-
 }
 
 void setupMQTT() {
+  // Initialize MQTT client and set callback function
   client.setServer(MQTT_HOST, MQTT_PORT);
   client.setCallback(MQTT_callback);
 }
 
 void loopMQTT() {
-
-  for (int i = 0; i < MAX_CONNECTION_RETRIES && !client.connected(); i++) {
-    MQTT_reconnect();
-    Serial.print(".");
-    MQTT_reconnect();
-  }
-
+  // Ensure MQTT connection is maintained
+  MQTT_reconnect();
+  
+  // Process MQTT messages
   client.loop();
+  
+  // Publish status to MQTT
   client.publish(mqttStatusTopic, gStatusAsJson.c_str());
 }
 
-#endif ENABLE_MQTT
+#endif // ENABLE_MQTT
